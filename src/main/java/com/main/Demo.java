c36b401be2993ff9a154551840d7c981a61845d1
vaishalivasu <vaishali.v1920@gmail.com>
2025-07-18 15:19:10 +0530

    commit1

---
+++ src/main/java/com/main/Demo.java
@@ -0,0 +1,10 @@
+package com.main;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
---
+++ src/test/java/com/test/Sample.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+
+}
